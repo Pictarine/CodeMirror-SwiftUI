7f0610fc28f24eefcb482a34930e5d7f425feb14
Anthony Fernandez <anthony@pictarine.com>
2020-09-24 22:58:43 +0200

    Add linewrapping option to macOS demo

--- Demo-macOS/Demo-macOS/ContentView.swift
+++ Demo-macOS/Demo-macOS/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
   @State private var selectedTheme = 0
   @State private var fontSize = 12
   @State private var showInvisibleCharacters = true
+  @State private var lineWrapping = true
   
   private var themes = CodeViewTheme.allCases.sorted {
     return $0.rawValue < $1.rawValue
@@ -30,6 +31,10 @@ struct ContentView: View {
           }
         }
         .frame(minWidth: 100, idealWidth: 150, maxWidth: 150)
+        
+        Spacer()
+        
+        Button(action: { lineWrapping.toggle() }) { Text("Wrap") }
         
         Spacer()
         
@@ -64,7 +69,8 @@ struct ContentView: View {
                    code: $codeBlock,
                    mode: codeMode,
                    fontSize: fontSize,
-                   showInvisibleCharacters: showInvisibleCharacters)
+                   showInvisibleCharacters: showInvisibleCharacters,
+                   lineWrapping: lineWrapping)
             .onLoadSuccess {
               print("Loaded")
             }
